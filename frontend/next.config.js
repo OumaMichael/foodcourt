234a92efca0fc2781084e46abaccb46d34074d8f
OumaMichael <oumamichael108@gail.com>
2025-08-31 16:31:12 +0300

    Prepare Deploy branch for Vercel frontend deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { 
+  images: {
     unoptimized: true,
-    domains: ['images.pexels.com'],
+    domains: ["images.pexels.com"],
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
 };
 
